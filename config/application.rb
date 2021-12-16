42545b1f0a8cc0120889a3b8263457bb2835c723
Kazuya-Sakashita <kz0508@gmail.com>
2021-12-16 20:35:46 +0900

    sidekiq bundle isntall

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module App
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.active_job.queue_adapter = :sidekiq
+    config.active_job.queue_name_prefix = Rails.env # これは任意
   end
 end
